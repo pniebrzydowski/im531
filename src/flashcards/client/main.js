1564226c1487eabf2223a6fa4a3d194ca0625bdb
Adwait Sharma <sharma.adwait@gmail.com>
2016-04-10 22:28:55 +0200

    Login system
    Each user can create/view his own word list only.

--- src/flashcards/client/main.js
+++ src/flashcards/client/main.js
@@ -14,7 +14,9 @@ if(Meteor.isServer){
 if(Meteor.isClient){
     Template.flashcard.helpers({
         'word': function(){
-        return WordsList.find({}, {sort: {score: -1, name: 1} })
+        var currentUserId = Meteor.userId();
+        return WordsList.find({creator: currentUserId},
+                            {sort: {score: -1, name: 1}});
     },
         'selectedClass': function(){
             var wordId = this._id;
@@ -47,15 +49,23 @@ if(Meteor.isClient){
     var selectedWord = Session.get('selectedWord');
     WordsList.update(selectedWord, {$inc: {score: -5} });
     },
+
+    'click .remove': function(){
+    var selectedWord = Session.get('selectedWord');
+    WordsList.remove(selectedWord);
+}
+
 });
 
     Template.addWordForm.events({
     'submit form': function(event){
      event.preventDefault();
     var wordNameVar = event.target.wordName.value;
+    var currentUserId = Meteor.userId();
     WordsList.insert({
           name: wordNameVar,
-          score: 0
+          score: 0,
+          creator: currentUserId
       });
     }
   });
--- src/flashcards/server/main.js
+++ src/flashcards/server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 WordsList = new Mongo.Collection('words');
+
+//UserAccounts = new Mongo.Collection('users');
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
