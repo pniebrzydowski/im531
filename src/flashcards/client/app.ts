40fee11fc88d14243e4ff7dc47b426f72e4c9442
Patrick Niebrzydowski <patrick.niebrzydowski@students.fh-hagenberg.at>
2016-04-17 23:50:15 +0200

    Connect back-end db with angular front-end

--- src/flashcards/client/app.ts
+++ src/flashcards/client/app.ts
@@ -1,12 +1,21 @@
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import {Component} from 'angular2/core';
-import {bootstrap} from 'angular2/platform/browser';
+import {bootstrap} from 'angular2-meteor-auto-bootstrap';
+import {Words} from '../collections/words';
+import {Tracker} from 'meteor/tracker';
  
 @Component({
   selector: 'app',
   templateUrl: 'client/app.html'
 })
-class Flashcards { }
+
+class Flashcards {
+  words: Array<Object>;
+  
+  constructor () {
+    this.words = Words.find();
+  }
+}
  
 bootstrap(Flashcards);
