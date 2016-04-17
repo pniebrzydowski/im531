bbe059ce8ab8b7c8281372876de3c389c00c7388
Patrick Niebrzydowski <patrick.niebrzydowski@students.fh-hagenberg.at>
2016-04-18 00:23:12 +0200

    Add server integration for add/delete words

--- src/flashcards/client/app.ts
+++ src/flashcards/client/app.ts
@@ -3,18 +3,23 @@ import 'zone.js/dist/zone';
 import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2-meteor-auto-bootstrap';
 import {Words} from '../collections/words';
-import {Tracker} from 'meteor/tracker';
+import {AddWordForm} from './components/add-word-form/add-word-form';
  
 @Component({
   selector: 'app',
-  templateUrl: 'client/app.html'
+  templateUrl: 'client/app.html',
+  directives: [AddWordForm]
 })
 
 class Flashcards {
-  words: Array<Object>;
+  words: Mongo.Cursor<Object>;
   
   constructor () {
     this.words = Words.find();
+  }
+  
+  removeWord(word) {
+    Words.remove(word._id);
   }
 }
  
---
+++ src/flashcards/client/components/add-word-form/add-word-form.ts
@@ -0,0 +1,33 @@
+import 'reflect-metadata';
+import {Component} from 'angular2/core';
+import {FormBuilder, ControlGroup, Validators, Control} from 'angular2/common';
+import {Words} from '../../../collections/words.ts';
+
+ 
+@Component({
+  selector: 'add-word-form',
+  templateUrl: '/client/components/add-word-form/add-word-form.html'
+})
+export class AddWordForm {
+  partiesForm: ControlGroup;
+ 
+  constructor() {
+    let fb = new FormBuilder();
+ 
+    this.addWordForm = fb.group({
+      name: ['',Validators.required]
+    });
+  }
+  
+  addWord(word) {
+    if (this.addWordForm.valid) {
+      Words.insert({
+        name: word.name,
+        score: 0,
+        creator: 'a@abc.com'
+      });
+ 
+      (<Control>this.addWordForm.controls['name']).updateValue('');
+    }
+  }
+}
