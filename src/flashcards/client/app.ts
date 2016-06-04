8586969b709b11d0d15f5d8dc7196c53223f2252
Adwait Sharma <sharma.adwait@gmail.com>
2016-06-04 14:30:58 +0200

    error handling and user auth

--- src/flashcards/client/app.ts
+++ src/flashcards/client/app.ts
@@ -5,6 +5,8 @@ import {bootstrap} from 'angular2-meteor-auto-bootstrap';
 import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig, APP_BASE_HREF} from 'angular2/router';
 
 import {LoginForm} from './components/login-form/login-form';
+import {ErrorPage} from './components/error-page/error-page';
+import {SignupForm} from './components/signup-form/signup-form';
 import {DeckList} from './components/deck-list/deck-list';
 import {AddDeckForm} from './components/add-deck-form/add-deck-form';
 import {DeckSettings} from './components/deck-settings/deck-settings';
@@ -17,13 +19,33 @@ import {ReviewWords} from './components/review-words/review-words';
 })
 
 @RouteConfig([
-	{ path: '/', as: 'DeckList', component: DeckList },
+	{ path: '/decks', as: 'DeckList', component: DeckList },
 	{ path: '/login', as: 'LoginForm', component: LoginForm },
+	{ path: '/signup', as: 'SignupForm', component: SignupForm },
 	{ path: '/create', as: 'AddDeckForm', component: AddDeckForm },
 	{ path: '/deck/:deckId', as: 'DeckSettings', component: DeckSettings },
-	{ path: '/review/:deckId', as: 'ReviewWords', component: ReviewWords }
+	{ path: '/review/:deckId', as: 'ReviewWords', component: ReviewWords },
+	{ path: '/*path', as: 'ErrorPage', component: ErrorPage }
+
 ])
 
-class Flashcards {}
+class Flashcards {
+	
+	constructor() {
+
+
+  } 
+
+
+}
  
 bootstrap(Flashcards, [ROUTER_PROVIDERS, provide(APP_BASE_HREF, { useValue: '/' })]);
+
+	Tracker.autorun(function(){
+if(Meteor.userId()==null && location.pathname!='/login' && location.pathname!='/signup' && !Meteor.userId()) {
+	    console.log('Oh yes');
+	    window.location.assign('/login');
+	  }
+	});
+
+
--- src/flashcards/client/components/login-form/login-form.ts
+++ src/flashcards/client/components/login-form/login-form.ts
@@ -1,23 +1,53 @@
 import 'reflect-metadata';
-import 'zone.js/dist/zone';
 import {Component} from 'angular2/core';
+import {Meteor} from 'meteor/meteor';
 import {Mongo} from 'meteor/mongo';
+import {FormBuilder, ControlGroup, Validators, Control} from 'angular2/common';
+import {Router} from 'angular2/router';
  
 @Component({
   selector: 'login-form',
   styleUrls: [
-  	'/client/components/login-form/style.less'],
+  	'/client/components/login-form/style.css'],
   templateUrl: '/client/components/login-form/login-form.html'
 })
 
 export class LoginForm {
-	currentTab;
+	loginForm: ControlGroup;
+	error: string;
 	
-	constructor () {
-		this.currentTab = 'login';
-	}
+	constructor(private router: Router) {
+		let fb = new FormBuilder();
+ 
+    	this.loginForm = fb.group({
+	      email: ['',Validators.required],
+	      password: ['',Validators.required]
+	    });
+
+	    this.error = '';
+  }
 	
-	setCurrentTab(tabName) {
-		this.currentTab = tabName;
-	}
+	login(credentials) {
+    if (this.loginForm.valid) {
+      Meteor.loginWithPassword(credentials.email, credentials.password, (err) => {
+        if (err) {
+          this.error = err;
+          console.log("Oh No");
+          document.getElementById("errorMessage").innerHTML = "Invalid Input";
+        }
+        else {
+          console.log("Oh yeah");
+          this.router.navigate(['/DeckList']);
+        }
+      });
+    }
+  }
 }
+
+
+
+
+
+
+
+
